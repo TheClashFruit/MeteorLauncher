7c44f8618b0e45432ce7d8d6ada53ed0f0bd793e
TheClashFruit <clash.fruit123@gmail.com>
2023-02-05 18:16:54 +0100

    feat: add drawables overrides for api 30 and down so it uses a default colour instead of material you

--- app/src/main/java/me/theclashfruit/launcher/MainActivity.kt
+++ app/src/main/java/me/theclashfruit/launcher/MainActivity.kt
@@ -3,8 +3,6 @@ package me.theclashfruit.launcher
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import me.theclashfruit.launcher.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
